cb4dc212119f69e12c666501f977a95e330d6780
NetoCh <54650291+NetoCh@users.noreply.github.com>
2022-07-10 06:38:13 +0000

    show extra parts

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import qs from 'query-string';
 import parse from 'html-react-parser';
 
@@ -19,14 +19,23 @@ import {
   Pagination,
   Popover,
   OverlayTrigger,
+  Button,
+  Accordion,
+  Modal,
 } from 'react-bootstrap';
 
 let request = "";
+let audio;
 function App() {
   const [loading, setLoading] = useState(true);
   const [dataId, setDataId] = useState(1);
   const [data, setData] = useState({});
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [show, setShow] = useState(false);
   const { id } = qs.parse(window.location.search);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const popover = (
     <Popover id="popover-basic">
@@ -36,8 +45,39 @@ function App() {
     </Popover>
   );
 
+
+  const extras = useCallback(() => {
+    if (!data?.extra_parts || !typeof data?.extra_parts === "object") return [];
+    return Object.values(data.extra_parts).map((value) => {
+      if (!value === "") return undefined;
+      return <>
+        {parse(`${value.replaceAll('href=\"', 'href=\"https://xkcd.com').replaceAll('src=\"', 'src=\"https://xkcd.com')}`)}
+      </>
+    }).filter(x => x !== undefined);
+  }, [data])
+
+
+
+  const handleExtraParts = function () {
+    handleShow();
+    // if (!audio) {
+    //   const audioURL = data.extra_parts.post.split("'")[3];
+    //   audio = new Audio(audioURL);
+    // }
+
+    // if (isAudioPlaying) {
+    //   audio.pause();
+    //   setIsAudioPlaying(false);
+    // } else {
+    //   audio.currentTime = 0;
+    //   audio.play();
+    //   setIsAudioPlaying(true);
+    // }
+  }
+
   const getData = async function (id = 1) {
     try {
+      if (audio) audio.pause();
       setLoading(true);
       const api = `/api/data?id=${id}`;
 
@@ -74,6 +114,17 @@ function App() {
                   <Card.Header><h3 className='text-center'>#{data.num} - {data.title}</h3></Card.Header>
                   <Card.Img variant="top" className='p-3 my-3 mx-auto shadow' style={{ maxWidth: '25rem', cursor: 'pointer' }} src={data.img} onClick={() => { if (data.link) window.open(data.link, '_blank') }} />
                   {data.link && <i className='text-center text-muted'><small>Info: <a href={data.link} target="_blank" rel="noreferrer">{data.link}</a></small></i>}
+                  {data.extra_parts &&
+                    <>
+                      <Row>
+                        <Col className='text-center'>
+                          <Button size="sm" variant={isAudioPlaying ? "danger" : "success"} onClick={() => {
+                            handleExtraParts();
+                          }}>Show Extras</Button>
+                        </Col>
+                      </Row>
+                    </>
+                  }
                   <Card.Body>
                     <Card.Title>{data.safe_title}</Card.Title>
                     <Card.Text><small className='text-muted'>{data.alt}</small></Card.Text>
@@ -95,6 +146,7 @@ function App() {
                         getData(0)
                       }} />
                     </Pagination>
+
                   </Card.Body>
                   <Card.Footer className="text-muted">
                     <Row>
@@ -118,6 +170,28 @@ function App() {
           </Col>
         </Row>
       </Container>
+      <Modal size="lg" centered show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Extra Parts</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row>
+            <Col>
+              {data.extra_parts &&
+                <>
+                  {extras().map((item) => {
+                    return item;
+                  })
+                  }
+                  <div id="comic"></div>
+                  <h5 className='mt-3'>JSON Data:</h5>
+                  <textarea className="w-100" rows={8} value={JSON.stringify(data.extra_parts, null, 2)} readOnly={true}></textarea>
+                </>
+              }
+            </Col>
+          </Row>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
